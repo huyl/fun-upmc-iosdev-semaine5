9b0ad23a3289700d25aa2043d968940c0281c421
huyl <huyl@users.noreply.github.com>
2014-05-29 21:52:06 -0500

    Initial Commit

---
+++ owned-MiniNav/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  owned-MiniNav
+//
+//  Created by Huy on 5/29/14.
+//  Copyright (c) 2014 huy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ owned-MiniNavTests/owned_MiniNavTests.m
@@ -0,0 +1,34 @@
+//
+//  owned_MiniNavTests.m
+//  owned-MiniNavTests
+//
+//  Created by Huy on 5/29/14.
+//  Copyright (c) 2014 huy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface owned_MiniNavTests : XCTestCase
+
+@end
+
+@implementation owned_MiniNavTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
